21c1d9e9de6db27083058934f6814d00f3ce6707
Bastien <contact@bastien27.net>
2025-01-23 18:27:39 +0100

    Update __init__.py
    add pico_ir. for import lib, need that load in raspberry pi pico 1

--- pico_ir/__init__.py
+++ pico_ir/__init__.py
@@ -1,3 +1,3 @@
-from read_code import read_code
-from send_code import send_code
-from validate_code import validate_code, InvalidCodeException
+from pico_ir.read_code import read_code
+from pico_ir.send_code import send_code
+from pico_ir.validate_code import validate_code, InvalidCodeException
